7d586d7152e5c5c7d545778ebe99a023cf9d3ea5
Gopikadas42 <gopikadazz@gmail.com>
2025-11-11 16:04:10 +0530

    1 commit in dev

--- Chat/consumers.py
+++ Chat/consumers.py
@@ -23,6 +23,9 @@ class ChatConsumers(AsyncWebsocketConsumer):
         )
     async def chat_message(self,event):
         message=event['message']
+        await self.send(text_data=json.dumps({
+            "message": message
+        } ))
 
 
 
